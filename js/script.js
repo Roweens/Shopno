588cdc3ac1daf5cd16aeed77e3a0ebaf754efd00
Roweens <enderman2011@gmail.com>
2020-07-26 23:26:41 +0300

    верстаю слайдер

--- js/script.js
+++ js/script.js
@@ -4,4 +4,13 @@ $(document).ready(function () {
 
 	});
 
+	$(".contacts__spoiler").click(function (event) {
+		if ($(".contacts__row").hasClass("one")) {
+			$(".contacts__spoiler").not($(this)).removeClass("active");
+			$(".contacts__spoiler-text").not($(this).next()).slideUp(300);
+		}
+		$(this).toggleClass("active").next().slideToggle(300);
+	});
+
+
 });
